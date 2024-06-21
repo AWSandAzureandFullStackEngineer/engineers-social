d4e4a1aea0dee6b70c18c7e6e640d2fa2e3b2c82
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-06-21 01:03:21 -0500

    fix.

---
+++ src/main/java/com/engineers/core/engineerssocial/EngineersSocialApplication.java
@@ -0,0 +1,13 @@
+package com.engineers.core.engineerssocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineersSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineersSocialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/engineers/core/engineerssocial/EngineersSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engineers.core.engineerssocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineersSocialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
