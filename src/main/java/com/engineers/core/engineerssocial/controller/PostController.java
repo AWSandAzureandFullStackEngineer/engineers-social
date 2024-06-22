e9a2219edf6f66aa19de8833e29ecf61d425185e
AWSandAzureandFullStackEngineer <awsandazurecloudengineer@gmail.com>
2024-06-22 18:46:00 -0500

    added azure dependency.

--- src/main/java/com/engineers/core/engineerssocial/controller/PostController.java
+++ src/main/java/com/engineers/core/engineerssocial/controller/PostController.java
@@ -1,7 +1,35 @@
 package com.engineers.core.engineerssocial.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.engineers.core.engineerssocial.entity.Post;
+import com.engineers.core.engineerssocial.response.ApiResponse;
+import com.engineers.core.engineerssocial.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/1.0/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping("/user/{userId}")
+    public ResponseEntity<Post> createPost(@RequestBody Post post, @PathVariable("userId") Integer userId) throws Exception {
+        Post createdPost = postService.createNewPost(post, userId);
+        return new ResponseEntity<>(createdPost, HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/posts/{postId}/user/{userId}")
+    public ResponseEntity<ApiResponse> deletePost(@PathVariable("postId") Integer postId,
+                                                  @PathVariable("userId") Integer userId) throws Exception {
+        String message = postService.deletePost(postId, userId);
+        ApiResponse response = new ApiResponse(message, true);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
